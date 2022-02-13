90aff45838004b89eb4204ac2e43fbdc3fbdcadd
Medina <imn.medina@icloud.com>
2022-02-14 00:05:47 +0600

    Initial commit

---
+++ src/com/company/Skeleton.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Skeleton extends Boss {
+    private int arrows;
+
+    public int getArrows() {
+        return arrows;
+    }
+
+    public void setArrows(int arrows) {
+        this.arrows = arrows;
+    }
+
+    public String printInfo(){
+        return super.printInfo() + "; Arrows" + this.arrows;
+    }
+}
